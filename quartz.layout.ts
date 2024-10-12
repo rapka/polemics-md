a50276c2df4ba6e77a9e080f8119feb322b7c793
college hill <572331+rapka@users.noreply.github.com>
2024-10-12 19:15:11 -0400

    Update layouts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -15,6 +15,43 @@ export const sharedPageComponents: SharedLayout = {
   }),
 }
 
+const sortFn = (a, b) => {
+  const dateA = a?.file?.dates?.created
+  const dateB = b?.file?.dates?.created
+
+  console.log('sroo', dateA)
+
+  return dateB - dateA
+}
+
+const mapFn = (node) => {
+  // dont change name of root node
+  if (node.depth > 0) {
+    // set emoji for file/folder
+    if (node.file) {
+      const date = node.file.dates?.created
+      const month = dateFormatter.format(date)
+      const year = date.getFullYear()
+
+      // node.displayName = `[${month} ${year}] ${node.displayName}`;
+      node.displayName = `${node.displayName}`
+    }
+  }
+}
+
+const explorerConfig = {
+  title: "writings", // title of the explorer component
+  folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
+  folderDefaultState: 'open', // default state of folders ("collapsed" or "open")
+  useSavedState: false, // wether to use local storage to save "state" (which folders are opened) of explorer
+  // Sort order: folders first, then files. Sort folders and files alphabetically
+  sortFn,
+  // filterFn: (node) => node.name !== "tags", // filters out 'tags' folder
+  mapFn,
+  // what order to apply functions in
+  order: ["filter", "map", "sort"],
+}
+
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
@@ -24,47 +61,18 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
+    Component.DesktopOnly(Component.PageTitle()),
     Component.DesktopOnly(Component.PageDescription()),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.Explorer({
-      title: "writings", // title of the explorer component
-      folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
-      folderDefaultState: 'open', // default state of folders ("collapsed" or "open")
-      useSavedState: true, // wether to use local storage to save "state" (which folders are opened) of explorer
-      // Sort order: folders first, then files. Sort folders and files alphabetically
-      sortFn: (a, b) => {
-        const dateA = a?.file?.dates?.created
-        const dateB = b?.file?.dates?.created
-
-        console.log('sroo', dateA)
-
-        return dateB - dateA
-      },
-      // filterFn: (node) => node.name !== "tags", // filters out 'tags' folder
-      mapFn: (node) => {
-        // dont change name of root node
-        if (node.depth > 0) {
-          // set emoji for file/folder
-          if (node.file) {
-            const date = node.file.dates?.created
-            const month = dateFormatter.format(date)
-            const year = date.getFullYear()
-
-             console.log('sroo', node.file)
-            // node.displayName = `[${month} ${year}] ${node.displayName}`;
-            node.displayName = `${node.displayName}`
-          }
-        }
-      },
-      // what order to apply functions in
-      order: ["filter", "map", "sort"],
-    }),
+    Component.DesktopOnly(Component.Search()),
+    Component.DesktopOnly(Component.PageTitle()),
+    Component.DesktopOnly(Component.Explorer(explorerConfig)),
+    Component.MobileOnly(Component.PageTitle()),
+    Component.MobileOnly(Component.Darkmode()),
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.MobileOnly(Component.Explorer(explorerConfig)),
   ],
 }
 
